60dc1f91788352e6dce8b61e6d05e2a283959106
scrummer <scrummer@gmx.ch>
2022-01-24 22:45:08 +0100

    Remove abstract middleware
    Signed-off-by: scrummer <scrummer@gmx.ch>

--- src/DiscordWebhookBundle/DiscordWebhook.php
+++ src/DiscordWebhookBundle/DiscordWebhook.php
@@ -3,11 +3,25 @@ declare(strict_types=1);
 
 namespace DiscordWebhookBundle;
 
+use DiscordWebhook\Webhook;
+use GuzzleHttp\Client;
+
 /**
  * Class DiscordWebhook
  *
  * @package DiscordWebhookBundle
  */
-class DiscordWebhook extends AbstractDiscordWebhook
+class DiscordWebhook extends Webhook
 {
+    public function __construct(array|string $url = [])
+    {
+        parent::__construct($url);
+    }
+
+    public function setUrl(string $webhook): void
+    {
+        $this->getClients()->add(new Client([
+            'base_uri' => $webhook
+        ]));
+    }
 }
